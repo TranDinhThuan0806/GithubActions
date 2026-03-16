3bfe53c09ee423a29b277f2ae50c0f6c41874674
tdt0862004@gmail.com <tdt0862004@gmail.com>
2026-03-16 14:45:24 +0700

    Github Action

---
+++ trandinhthuan/src/main/java/com/example/trandinhthuan/TrandinhthuanApplication.java
@@ -0,0 +1,13 @@
+package com.example.trandinhthuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrandinhthuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrandinhthuanApplication.class, args);
+	}
+
+}
---
+++ trandinhthuan/src/test/java/com/example/trandinhthuan/TrandinhthuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trandinhthuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrandinhthuanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
